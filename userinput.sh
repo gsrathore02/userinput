7fcbccad67a70fac2ee690f502980d7d1c68edf5
Gulab <gulab.r@sabsebolo.com>
2017-02-03 04:19:14 -0500

    six comment

--- userinput.sh
+++ userinput.sh
@@ -8,6 +8,7 @@ until [ "$selection" = "0" ]; do
     echo "2 - display free memory"
     echo "3 - current process"
     echo "4 - uptime"
+    echo "5 - grep command"
     echo "0 - exit program"
     echo ""
     echo -n "Enter selection: "
@@ -18,6 +19,7 @@ until [ "$selection" = "0" ]; do
         2 ) free ;;
         3 ) top ;;
         4 ) w ;;
+        5 ) netstat -nap | grep 8080;;
         0 ) exit ;;
         * ) echo "Please enter 1, 2, or 0"
     esac
